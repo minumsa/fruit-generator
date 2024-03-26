554363ba616fd5444a68ba731402aca2f0e9fa11
유일 <83910706+minumsa@users.noreply.github.com>
2024-03-26 11:10:58 +0900

    과일생성기 외 전부 삭제

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,30 @@
+import React from "react";
 import "./globals.css";
 
 export const metadata = {
-  title: "divdivdiv",
-  description: "재밌는 웹페이지를 만듭니다.",
+  title: "과일 생성기",
+  openGraph: {
+    images: [{ url: "/thumbnail.png", width: 1800, height: 945 }],
+    locale: "ko_KR",
+    siteName: "divdivdiv",
+    title: "과일 생성기",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    creator: "@dev_carver",
+    description: "화면의 특정 위치에 랜덤하게 과일이 생성됩니다.",
+    images: ["https://divdivdiv.com/fruits-thumbnail.png"],
+    title: "과일 생성기",
+  },
 };
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html>
       <body>{children}</body>
     </html>
   );
-}
+};
+
+export default Layout;
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,100 @@
 "use client";
 
-import { imgAltAtom, imgSrcAtom, showImageAtom } from "./modules/data";
-import DraggableIcons from "./components/DraggableIcons";
-import { ImageModal } from "./components/Modal";
-import { useAtom } from "jotai";
-import { FunctionalLayout } from "./components/FunctionalLayout";
+import { useEffect, useState } from "react";
+import { isMobile } from "react-device-detect";
+import { fruitEmojiMap } from "./modules/fruitData";
+import { NOSSR } from "./modules";
+import styles from "./fruits.module.css";
 
-export default function Page() {
-  const [showImage, setShowImage] = useAtom(showImageAtom);
-  const [imgSrc, setImgSrc] = useAtom(imgSrcAtom);
-  const [imgAlt, setImgAlt] = useAtom(imgAltAtom);
+function getRandomItem(arr: string[]): string {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
 
-  const handleModalClick = () => {
-    setShowImage(false);
-    setImgSrc("");
-    setImgAlt("");
+function Fruits() {
+  const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
+  const [checkerWidth, setCheckerWidth] = useState<number>(0);
+
+  const handleWindowResize = () => {
+    setWindowWidth(window.innerWidth);
   };
 
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowResize);
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  // FIXME: 리액트 코드로 최대한 바꿔보기
+  useEffect(() => {
+    const container = document.getElementById("container");
+
+    const interval = setInterval(() => {
+      const fruit = document.createElement("div");
+      const fruitsArr: string[] = Object.keys(fruitEmojiMap);
+      const randomFruit = getRandomItem(fruitsArr);
+      fruit.innerHTML = randomFruit;
+      fruit.style.left = `${Math.random() * 100}%`;
+      container?.appendChild(fruit);
+
+      // TODO: pointerEvents 검색해보기
+      fruit.style.pointerEvents = "auto";
+      fruit.style.cursor = "pointer";
+
+      const eventName = isMobile ? "touchstart" : "click";
+      const clickHandler = () => {
+        const fruitArray = fruitEmojiMap[randomFruit];
+        if (fruitArray) {
+          fruit.removeEventListener(eventName, clickHandler);
+          alert(getRandomItem(fruitArray));
+        }
+      };
+
+      fruit.addEventListener(eventName, clickHandler);
+
+      setTimeout(() => {
+        fruit.remove();
+      }, 5000);
+    }, 500);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (typeof window === "undefined") {
+      return;
+    }
+
+    if (isMobile) {
+      setCheckerWidth(windowWidth / 6);
+    } else {
+      setCheckerWidth(windowWidth / 12);
+    }
+  }, [windowWidth]);
+
   return (
-    <>
-      {showImage && <ImageModal src={imgSrc} alt={imgAlt} onClick={handleModalClick} />}
-      <FunctionalLayout>
-        <DraggableIcons />
-      </FunctionalLayout>
-    </>
+    <div
+      className={styles["container"]}
+      style={{
+        backgroundSize: `${checkerWidth * 2}px ${checkerWidth * 2}px`,
+        backgroundPosition: `0 0, 0 ${checkerWidth}px, ${checkerWidth}px -${checkerWidth}px, -${checkerWidth}px 0px`,
+      }}
+    >
+      <div
+        id="container"
+        style={{ fontSize: `${checkerWidth}px` }}
+        className={styles["falling-fruits"]}
+      ></div>
+    </div>
+  );
+}
+
+export default function Page() {
+  return (
+    <NoSSR>
+      <Fruits />
+    </NoSSR>
   );
 }
